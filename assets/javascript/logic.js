e03a5f2fd61c5d95b647da109b37452a6e769830
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-15 23:32:43 -0400

    finished all coding but firebase is not pushing yet

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,57 +1,58 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAcy9BBTZY96qJP3nhXau8uRSfIgjE6mxI",
-    authDomain: "aida-s-first.firebaseapp.com",
-    databaseURL: "https://aida-s-first.firebaseio.com",
-    projectId: "aida-s-first",
-    storageBucket: "aida-s-first.appspot.com",
-    messagingSenderId: "610842950040",
-    appId: "1:610842950040:web:76218a6419982d30"
+  apiKey: "AIzaSyAcy9BBTZY96qJP3nhXau8uRSfIgjE6mxI",
+  authDomain: "aida-s-first.firebaseapp.com",
+  databaseURL: "https://aida-s-first.firebaseio.com",
+  projectId: "aida-s-first",
+  storageBucket: "aida-s-first.appspot.com",
+  messagingSenderId: "610842950040",
+  appId: "1:610842950040:web:76218a6419982d30"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-var name = "";
-var destination = "";
-var firstTrain ;
-var frequency = 0;
+$(".btn btn-primary").on("click", function (event) {
+  event.preventDefault();
 
+  var name = $("#trainInput").val().trim();
+  var destination = $("#destinationInput").val().trim();
+  var frequency = moment($("#frequencyInput").val().trim(), "minutes");
+  var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm");
 
-$(".btn-primary").on("click", function (event) {
-    event.preventDefault();
-    name = $("#trainInput").val().trim();
-    destination = $("#destinationInput").val().trim();
-    firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(1, "years");
-    console.log(firstTrain);
-    frequency = $("#frequencyInput").val().trim();
-    
+  var newTrain = {
+    name: name,
+    destination: destination,
+    frequency: frequency,
+    firstTrain: firstTrain
+  };
+  database.ref().push(newTrain);
 
+  alert("New train info successfully added");
 
-      database.ref().push({
-        name: name,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency
-  });
-      });
+  $("#trainInput").val("");
+  $("#destinationInput").val("");
+  $("#frequencyInput").val("");
+  $("#firstTrainInput").val("");
+});
+database.ref().on("child_added", function (childSnapshot) {
+  var name;
+  var destination;
+  var frequency;
+  var nextTrain;
+  var minutesAway;
+  var firstTrainfirst = moment(childSnapshot.val().firstTrain, "MM:hh").subtract(1, "years");
+  var timeDiff = moment().diff(moment(firstTrainfirst), "minutes");
+  var remainder = timeDiff % childSnapshot.val().frequency;
+  var minutesAway = childSnapshot.val().frequency - remainder;
+  var nextTrain = moment().add(minutesAway, "minutes");
+  nextTrain = moment(nextTrain).format("HH:mm");
 
-database.ref().on("child_added", function (snapshot) {
-    var sv = snapshot.val();
-    var minAwayData = $("<td>");
-    var newRow = $("<tr>");
-    var nameData = $("<td>");
-    nameData.text(sv.name);
-    var destinationData = $("<td>");
-    destinationData.text(sv.destination);
-    var diffData = $("<td>");
-    diffData.text(moment().diff(moment(earlyTrain), "minutes"));
-    var earlyTrain = $("<td>");
-    earlyTrain.text(moment().diff(moment(sv.date, "HH:mm"), "minutes"));
-    var nextTrainData = moment().add(minAwayData, "minutes");
-    nextTrainData = moment(nextTrainData).format("HH:mm");
-    console.log(snapshot);
+  var newRow = $("<tr>").append(
+    $("<td>").text(name),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(minutesAway),
+    $("<td>").text(nextTrain)
+  );
+  $("#trainTable > tbody").append(newRow);
 
-    newRow.append(nameData, destinationData, diffData, nextTrainData, minAwayData);
-    $("tbody").append(newRow);
-
-   
 });
